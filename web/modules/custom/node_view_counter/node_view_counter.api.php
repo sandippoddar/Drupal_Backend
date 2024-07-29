b8ed68b60c1f2be92dbead4e19fba0185a1b68a6
sandippoddar <sandip.poddar@innoraft.com>
2024-07-29 15:48:49 +0530

    FT2-234: Added a Custom Module to show how many times a node has been viewed in a Session.

---
+++ web/modules/custom/node_view_counter/node_view_counter.api.php
@@ -0,0 +1,19 @@
+
+<?php
+
+/**
+ * @file
+ * Hooks specific to the Hooks Example module.
+ */
+
+/**
+ *  This hook Display the count of time a node is Viewed in Session.
+ *
+ * @param int $current_count
+ *   The number of times that the current user has viewed the node during this
+ *   session.
+ */
+function hook_active_count(int $current_count) {
+  //Implementation would be done by other modules.
+} 
+          
